21c06e62166513189b38243f6aa9053bfb0ae7d2
Alan RL <alan.r.l.4018@gmail.com>
2020-10-18 08:31:17 -0500

    Cambios

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
 export default {
     IP: "104.238.138.234",
     user: "keyvox-webapp",
-    password: "gy/WilBtFFQouaPYo2OKeg==",
+    password: "7Kb443PWqFBP5iO84pnSYA==",
     port: "27017",
     name: "keyvox-test"
 }
--- src/controllers/API/validation.ts
+++ src/controllers/API/validation.ts
@@ -1,64 +1,64 @@
-import express from 'express'
-import ClientModel, { IClient } from '../../models/client'
-import { checkUserNotAuthenticated } from '../../middlewares'
-import { Payload, createToken } from '../../util'
-const router = express.Router()
+import express from 'express';
+import ClientModel, { IClient } from '../../models/client';
+import { checkUserNotAuthenticated } from '../../middlewares';
+import { Payload, createToken } from '../../util';
+const router = express.Router();
 
-router.post("/login", checkUserNotAuthenticated, async (req, res) => {
+router.post('/login', checkUserNotAuthenticated, async (req, res) => {
 	try {
 		let doc: IClient | null;
-		doc = null
+		doc = null;
 		if (req.body.username && req.body.password) {
 			doc = await ClientModel.findOne({
 				username: req.body.username,
-				password: req.body.password
-			})
+				password: req.body.password,
+			});
 			if (doc) {
 				res.status(200).send({
-					token: createToken(doc._id, doc.secretKey)
-				})
+					token: createToken(doc._id, doc.secretKey),
+				});
 			} else {
-				res.status(404).end('No se pudo autenticar')
+				res.status(404).end('No se pudo autenticar');
 			}
-		}
-		else {
+		} else {
 			if (req.body.publicKey) {
 				doc = await ClientModel.findOne({
-					publicKey: req.body.publicKey
-				})
+					publicKey: req.body.publicKey,
+				});
 				if (doc) {
 					res.status(200).send({
-						token: createToken(doc._id, doc.publicKey)
-					})
+						token: createToken(doc._id, doc.publicKey),
+					});
 				} else {
-					res.status(404).end('No se pudo autenticar')
+					res.status(404).end('No se pudo autenticar');
 				}
 			} else if (req.body.privateKey) {
 				doc = await ClientModel.findOne({
-					privateKey: req.body.privateKey
-				})
+					privateKey: req.body.privateKey,
+				});
 				if (doc) {
 					res.status(200).send({
-						token: createToken(doc._id, doc.secretKey)
-					})
+						token: createToken(doc._id, doc.secretKey),
+					});
 				} else {
-					res.status(404).end('No se pudo autenticar')
+					res.status(404).end('No se pudo autenticar');
 				}
 			}
 		}
 		if (!doc) {
-			res.status(500).end("No se recibio ningun valor para obtener el token")
+			res.status(500).end(
+				'No se recibio ningun valor para obtener el token'
+			);
 		}
-
+	} catch (err) {
+		console.log(err);
+		res.status(500).send(err);
 	}
-	catch (err) {
-		res.status(500).send(err)
-	}
-})
+});
 
 /**
  * idValidationRequest
  * Record
- * 
+ *
  */
 export default router;
